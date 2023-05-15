ea219316359de38b5cee501d53423a0e69f5012e
d22004772 <adam.DEANGELI@etu.univ-amu.fr>
2023-05-15 17:28:23 +0200

    exo 5 suite

--- src/main/java/com/example/partie2/exercice5/JeuMain.java
+++ src/main/java/com/example/partie2/exercice5/JeuMain.java
@@ -1,5 +1,6 @@
 package com.example.partie2.exercice5;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -19,6 +20,11 @@ public class JeuMain extends Application {
 
     private static ArrayList<Obstacle> Obstacles;
 
+    private double j1X;
+    private double j1Y;
+    private double j2X;
+    private double j2Y;
+
     @Override
     public void start(Stage primaryStage) {
 
@@ -28,8 +34,10 @@ public class JeuMain extends Application {
         Personnage pacman = new Pacman();
         Personnage fantome = new Fantome();
         Obstacle LeObstacle = new Obstacle(220,210,60,270);
+        Obstacle Le2Obstacle = new Obstacle(450,0,60,270);
         Obstacles = new ArrayList<>();
         Obstacles.add(LeObstacle);
+        Obstacles.add(Le2Obstacle);
         // on positionne le fantôme 20 positions vers la droite
         fantome.setLayoutX(20 * 10);
         //panneau du jeu
@@ -60,6 +68,11 @@ public class JeuMain extends Application {
      */
     private void deplacer(Personnage j1, Personnage j2, ArrayList<Obstacle> Obstacles) {
         scene.setOnKeyPressed((KeyEvent event) -> {
+             j1X = j1.getLayoutX();
+             j1Y = j1.getLayoutY();
+             j2X = j2.getLayoutX();
+             j2Y = j2.getLayoutY();
+
             switch (event.getCode()) {
                 case LEFT:
                     j1.deplacerAGauche();
@@ -90,18 +103,25 @@ public class JeuMain extends Application {
 
             }
             if (j1.estEnCollision(j2)) {
-                System.out.println("Game Over");
-                Label GameOver = new Label("Game Over");
+                System.out.println("Joueur 1 a gagné");
+                Label GameOver = new Label("Joueur 1 a Gagné");
                 GameOver.setFont(new Font(40));
                 BorderPane.setAlignment(GameOver, Pos.CENTER);
                 root.setCenter(GameOver);
             }
 
-            for (int i; i < Obstacles.size(); ++i){
+            for (int i = 0; i < Obstacles.size(); ++i){
+                if (Obstacles.get(i).estEnCollision(j1)){
+                    System.out.println("Obstacle j1");
+                    j1.setLayoutX(j1X);
+                    j1.setLayoutY(j1Y);
+                }
+                if (Obstacles.get(i).estEnCollision(j2)){
+                    System.out.println("Obstacle j2");
+                    j2.setLayoutX(j2X);
+                    j2.setLayoutY(j2Y);
 
-            }
-            if (){
-                System.out.println("Obstacle");
+                }
             }
 
 
